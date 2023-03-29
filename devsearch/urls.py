324931a3869eaa273ceeb0c1486af17baa2f42e3
MadhavReddy <108754603+madhavreddy509@users.noreply.github.com>
2023-03-29 18:17:37 +0530

    Update urls.py

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('api/',include('api.urls')),
+#     path('api/',include('api.urls')),
     path('admin/', admin.site.urls),
     path('projects/',include('projects.urls')),
     path('',include('users.urls')),
